828693b51b1b48da8dee5a111b4571df99ba062a
Matthew Lee <yleematthew@gmail.com>
2018-12-20 19:54:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,14 +7,24 @@ function dwarfRollCall(dwarves) {
 }
 
 
+//function summonCaptainPlanet(planeteerCalls){
+//  var array1=[]
+//  var punctuation="!"
+//  for (let i=0;i<planeteerCalls.length;i++){
+//    array1.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase())
+//  }
+//  return array1
+//}
+
 function summonCaptainPlanet(planeteerCalls){
   var array1=[]
   var punctuation="!"
   for (let i=0;i<planeteerCalls.length;i++){
-    array1.push(`${planeteerCalls[i]}${punctuation}`.toUpperCase())
+    array1.push(planeteerCalls[i].toUpperCase()+"!")
   }
   return array1
 }
+
 
 function longPlaneteerCalls(words) {
   for(let i = 0; i < words.length; i++){
@@ -26,4 +36,59 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
+  for(let i=0;i<foods.length;i++){
+    if (foods[i] === "gouda" || foods[i]=== "cheddar" || foods[i]=== "camembert"){
+    return foods[i]
+  }
 }
+    return "no cheese!"
+}
+
+
+function findTheCheese (foods) {
+	var cheeses = ["cheddar", "gouda", "camembert"];
+	for (var i = 0; i < foods.length; i++) {
+		for (var j = 0; j < cheeses.length; j++) {
+			if ( cheeses[j]=== foods[i] ) {
+				return foods[i]; //#will quit and return the first cheese that it finds
+			}
+		}
+	}
+	return "no cheese!"; //#returns "no cheese!" if it doesn't find any cheese :(
+}
+
+
+function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camambert"]
+  for (let i=0; i < foods.length; i++) {
+    if (foods[i] == cheese[i]) {
+      return (`${cheese[i]}`) }
+  else {
+       return "no cheese!"
+      }
+  }
+}
+
+
+
+function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camambert"]
+  for (let i=0; i < foods.length; i++) {
+    if (foods.includes(cheese[i])) {
+      return (`${cheese[i]}`) }
+  else {
+       return "no cheese!"
+      }
+  }
+}
+
+
+function findTheCheese (foods) {
+  var cheese = ["cheddar", "gouda", "camambert"]
+  for (let i=0; i < foods.length; i++) {
+    if (foods.includes(cheese[i])) {
+      return cheese[i] //no need to interpolate with backtics as this amounts to the same thing
+  } //end of if block
+} //end of for loop
+  return 'No cheese found.' //outside of for loop, so entire loop can execute
+  } //end of function block
